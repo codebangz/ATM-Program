4cc41bc58d0ceded71d7d3c800be5124e0d2413f
Akerekan Olakunle Michael <olakunlebangz@gmail.com>
2019-11-24 20:06:55 +0200

    Update atm.cpp

--- atm.cpp
+++ atm.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
 int main() {
-    double balance, withdraw, deposit;  //The ATM Functions declared as variables
+    double balance, withdraw, deposit; 
     balance =0; //Balance variable being set to initial 0
     int input;
 
-    cout <<"1. Check your balance \n"
+    cout <<"1. Check your balance \n" 
          <<"2. Deposit money \n"
          <<"3. Make a withdrawal \n"
-         <<"4. Exit. Enter 00 to exit \n";
+         <<"4. Exit. Enter 00 to exit \n"; 
          cin >> input;
 
          while(input!= -00){
             switch (input){
                 case 1 : cout << " The current balance in your account is " << balance<< " euros" <<endl;
                 break;
-                case 2: cout << " Enter an amount you wish to add to your account" ;
+                case 2: cout << " Enter an amount you wish to deposit into your account" ;
                 cin>>deposit;
                 balance= balance + deposit;
                 cout << "You have deposited " << deposit<< " euros into your account" <<endl;
